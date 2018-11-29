7c62098ad5578f3804f6baca292acde1fc7fd8fc
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-29 19:04:29 +0000

    Automatically backed up by Learn

---
+++ cli-gem-portfolio/lib/cli/gem/portfolio.rb
@@ -0,0 +1,10 @@
+require "cli/gem/portfolio/version"
+
+module Cli
+  module Gem
+    module Portfolio
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ cli-gem-portfolio/lib/cli/gem/portfolio/version.rb
@@ -0,0 +1,7 @@
+module Cli
+  module Gem
+    module Portfolio
+      VERSION = "0.1.0"
+    end
+  end
+end
